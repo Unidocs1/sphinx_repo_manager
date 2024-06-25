a3c27754b1d7456ae007fac7886ef15aaaeb3866
Dylan Hunt <d.hunt@xsolla.com>
2024-06-25 17:23:36 +0800

    feat: Separate toctrees with same caption = merged

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -183,6 +183,8 @@ pygments_style = "sphinx"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named 'default.css' will overwrite the builtin 'default.css'.
 html_static_path = ['_static']
+def setup(app):
+    app.add_css_file(os.path.normpath('styles/main.css'))
 
 html_logo = 'https://docs.xsolla.cloud/en/latest/_static/logo.png'
 html_favicon = 'https://docs.xsolla.cloud/en/latest/_static/favicon.ico'
@@ -200,7 +202,7 @@ html_theme_options = {
     # Toc options >>
     'collapse_navigation': False,
     'sticky_navigation': True,
-    'navigation_depth': 2,  # (!) max depth; NOT default
+    'navigation_depth': 2, # (!) Important
     'includehidden': True,
     'titles_only': False,
 }
