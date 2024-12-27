f4a5f7f8509d90a882e467caa1063d8a5e3dfd58
Dylan Hunt <d.hunt@xsolla.com>
2024-12-27 13:13:34 +0800

    fix(__init__.py): warnings, stability, typings

--- sphinx_repo_manager/__init__.py
+++ sphinx_repo_manager/__init__.py
@@ -3,18 +3,17 @@ from sphinx.config import Config
 from .sphinx_repo_manager import SphinxRepoManager
 from .git_helper import GitHelper
 from .api_reference import configure_doxygen
-from typing import TypedDict
 from pathlib import Path
-from typing import cast
 import importlib.metadata
 
 # Get the version of the extension
 try:
     __version__ = importlib.metadata.version(__name__)
 except importlib.metadata.PackageNotFoundError:
-    __version__ = "0.0.0" 
+    __version__ = "0.0.0"
 
-class SphinxRepoManagerConfig(TypedDict):
+
+class SphinxRepoManagerConfig:
     """
     Define the type structure for sphinx_repo_manager config values.
 
@@ -29,38 +28,52 @@ class SphinxRepoManagerConfig(TypedDict):
         start_time (float): The start time of the build.
         end_time (float): The end time of the build.
         manifest (dict): The repository manager manifest
+        raw_manifest (dict): The raw repository manager manifest
     """
-    repo_manager_manifest_path: Path
-    read_the_docs_build: bool
-    source_static_path: Path
-    source_doxygen_path: Path
-    default_repo_auth_user: str
-    default_repo_auth_token: str
-    has_default_repo_auth_token: bool
-    start_time: float
-    end_time: float
-    manifest: dict
-    # raw_manifest: dict
 
     def __init__(self):
         # TODO: Config should be defaulted by env vars in this constructor,
         # and then a new instance should be set as the default config value
         # in `setup` below. This way, the config values can be set in conf.py
-        # and then loaded to the SphinxMnanager instance via its constructor
+        # and then loaded to the SphinxManager instance via its constructor
         # during `create_repo_manager`.
-        self.repo_manager_manifest_path = Path()
-        self.read_the_docs_build = False
-        self.source_static_path = Path()
-        self.source_doxygen_path = Path()
-        self.default_repo_auth_user = ""
-        self.default_repo_auth_token = ""
-        self.has_default_repo_auth_token = False
-        self.start_time = 0.0
-        self.end_time = 0.0
-        self.manifest = {}
-        # self.raw_manifest = {}
+        self.repo_manager_manifest_path: Path = Path()
+        self.read_the_docs_build: bool = False
+        self.source_static_path: Path = Path()
+        self.source_doxygen_path: Path = Path()
+        self.default_repo_auth_user: str = ""
+        self.default_repo_auth_token: str = ""
+        self.has_default_repo_auth_token: bool = False
+        self.start_time: float = 0.0
+        self.end_time: float = 0.0
+        self.manifest: dict = {}
+        self.raw_manifest: dict = {}
 
-def set_config_dot_py_vals(app: Sphinx, config: Config, repo_manager: SphinxRepoManager):
+    def set_from_repo_manager(self, repo_manager: SphinxRepoManager):
+        """
+        Set the configuration values from a SphinxRepoManager instance.
+
+        Args:
+            repo_manager (SphinxRepoManager): The repository manager to extract values from.
+        """
+        self.repo_manager_manifest_path = repo_manager.repo_manager_manifest_path
+        self.read_the_docs_build = repo_manager.read_the_docs_build
+        self.source_static_path = repo_manager.source_static_path
+        self.source_doxygen_path = repo_manager.source_doxygen_path
+        self.default_repo_auth_user = repo_manager.env_repo_auth_user
+        self.default_repo_auth_token = repo_manager.env_repo_auth_token
+        self.has_default_repo_auth_token = repo_manager.has_env_repo_auth_token
+        self.start_time = repo_manager.start_time
+        self.end_time = repo_manager.end_time
+        self.manifest = repo_manager.manifest
+        self.raw_manifest = repo_manager.raw_manifest
+
+
+def set_config_dot_py_vals(
+        app: Sphinx,
+        config: Config,
+        repo_manager: SphinxRepoManager,
+):
     """
     Save useful config vals to be accessible from conf.py.
 
@@ -70,7 +83,7 @@ def set_config_dot_py_vals(app: Sphinx, config: Config, repo_manager: SphinxRepo
         repo_manager (SphinxRepoManager): The Sphinx repository manager instance containing the configuration values to be saved.
     """
     # TODO: We should avoid modifying `config` values as a way to expose data to external modules. Config
-    # should be treated as immutable after config is initailized.
+    # should be treated as immutable after config is initialized.
     # 
     # For setting versioned data as environment domains, we should use either the env collector or domain API
     #   - https://www.sphinx-doc.org/en/master/extdev/appapi.html#sphinx.application.Sphinx.add_env_collector
@@ -79,19 +92,11 @@ def set_config_dot_py_vals(app: Sphinx, config: Config, repo_manager: SphinxRepo
     # For firing off custom events to be caught by other modules, we should use add_event and emit:
     #   - https://www.sphinx-doc.org/en/master/extdev/appapi.html#sphinx.application.Sphinx.add_event
     #   - https://www.sphinx-doc.org/en/master/extdev/appapi.html#emitting-events
-    config.sphinx_repo_manager = cast(SphinxRepoManagerConfig, {
-        "repo_manager_manifest_path": repo_manager.repo_manager_manifest_path,
-        "source_static_path": repo_manager.source_static_path,
-        "read_the_docs_build": repo_manager.read_the_docs_build,
-        "source_doxygen_path": repo_manager.source_doxygen_path,
-        "default_repo_auth_user": repo_manager.env_repo_auth_user,
-        "default_repo_auth_token": repo_manager.env_repo_auth_token,
-        "has_default_repo_auth_token": repo_manager.has_env_repo_auth_token,
-        "start_time": repo_manager.start_time,
-        "end_time": repo_manager.end_time,
-        "manifest": repo_manager.manifest,
-        "raw_manifest": repo_manager.raw_manifest,
-    })
+
+    # Create a SphinxRepoManagerConfig instance -> set values from repo_manager
+    config.sphinx_repo_manager = SphinxRepoManagerConfig()
+    config.sphinx_repo_manager.set_from_repo_manager(repo_manager)
+
 
 def create_repo_manager(app: Sphinx, config: Config):
     """
@@ -100,12 +105,15 @@ def create_repo_manager(app: Sphinx, config: Config):
     Args:
         app (Sphinx): The Sphinx application instance.
         config (Config): The Sphinx configuration object.
-        repo_manager (SphinxRepoManager): The Sphinx repository manager instance containing the configuration values to be saved.
     """
-    # Instantiate the SphinxRepoManager
-    repo_manager = SphinxRepoManager(app)
-    set_config_dot_py_vals(app, config, repo_manager)
-    configure_doxygen(app, config, repo_manager)
+    try:
+        # Instantiate the SphinxRepoManager
+        repo_manager = SphinxRepoManager(app)
+        set_config_dot_py_vals(app, config, repo_manager)
+        configure_doxygen(app, config, repo_manager)
+    except Exception as e:
+        print(f"[sphinx_repo_manager] WARNING: Failed to initialize: {e}")
+
 
 def setup(app: Sphinx):
     """
@@ -118,10 +126,20 @@ def setup(app: Sphinx):
         dict: The extension metadata. See https://www.sphinx-doc.org/en/master/extdev/index.html#extension-metadata
     """
     # Add repo manager manifest path to the Sphinx config - changes to this will trigger a full rebuild
-    app.add_config_value("repo_manager_manifest_path", Path(app.confdir, "..", "repo_manifest.yml").resolve(), "env", [Path, str])
-    
+    abs_yml_path = Path(app.confdir, "..", "repo_manifest.yml").resolve()
+    app.add_config_value(
+        "repo_manager_manifest_path",
+        abs_yml_path,
+        "env",
+        [Path, str],
+    )
+
     # Add repo manager config - no changes to this will trigger a rebuild. Default values are set in the constructor.
-    app.add_config_value("sphinx_repo_manager", SphinxRepoManagerConfig(), "", SphinxRepoManagerConfig)
+    app.add_config_value(
+        "sphinx_repo_manager",
+        SphinxRepoManagerConfig(),
+        "",
+        SphinxRepoManagerConfig)
 
     # Connect to the config-inited event -> and only then instantiate SphinxRepoManager.
     # This fires before `builder-inited` and `env-updated` events.
